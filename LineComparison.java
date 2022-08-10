820bc62bf9e2db771a5b185a9845e1f5f4684766
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-10 19:51:35 +0530

    Added : To Check Lines Are Equals Or Not

--- LineComparison.java
+++ LineComparison.java
@@ -1,15 +1,25 @@
-package UC1;
+package UC2;
 
 import java.util.Scanner;
 
 public class LineComparison {
-    public static double lengthOfLine(int x1, int y1, int x2, int y2) {
-        return Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    static void checkEquality(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+        Double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length Of First Line : "+length1);
+
+        Double length2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+        System.out.println("Length Of Second Line : "+length2);
+
+        if (length1.equals(length2)) {
+            System.out.println("Both the lines are equal");
+        } else {
+            System.out.println("lines are not equal");
+        }
     }
     public static void main(String[] args) {
-        System.out.println("Enter The Value For Co-ordinates");
         Scanner sc = new Scanner(System.in);
-        System.out.print("x1 : ");
+        System.out.println("Enter the co-ordinates for length of first line");
+        System.out.print("x1 = ");
         int x1 = sc.nextInt();
         System.out.print("y1 : ");
         int y1 = sc.nextInt();
@@ -17,6 +27,16 @@ public class LineComparison {
         int x2 = sc.nextInt();
         System.out.print("y2 : ");
         int y2 = sc.nextInt();
-        System.out.println("Length of line : "+lengthOfLine(x1, y1, x2, y2));
+
+        System.out.println("Enter the co-ordinates for length of second line");
+        System.out.print("x3 : ");
+        int x3 = sc.nextInt();
+        System.out.print("y3 : ");
+        int y3 = sc.nextInt();
+        System.out.print("x4 : ");
+        int x4 = sc.nextInt();
+        System.out.print("y4 : ");
+        int y4 = sc.nextInt();
+        checkEquality(x1, y1, x2, y2, x3, y3, x4, y4);
     }
 }
