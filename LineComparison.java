b79b80212d3cbb12ffeefd821f35a62637156dc2
bharatpathak1998 <bharatpathak099@gmail.com>
2022-08-10 20:32:38 +0530

    Added : To Check Lines By Using CompareTo Method

--- LineComparison.java
+++ LineComparison.java
@@ -1,19 +1,22 @@
-package UC2;
+package UC3;
 
 import java.util.Scanner;
 
 public class LineComparison {
-    static void checkEquality(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        Double length1 = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-        System.out.println("Length Of First Line : "+length1);
+    static void checkComparison(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
+        Double length1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Length Of First Line : " + length1);
 
-        Double length2 = Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
-        System.out.println("Length Of Second Line : "+length2);
+        Double length2 = Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Length Of Second Line : " + length2);
 
-        if (length1.equals(length2)) {
-            System.out.println("Both the lines are equal");
+        int compare = length1.compareTo(length2);
+        if (compare < 0) {
+            System.out.println("Length of first line is less than length of second line");
+        } else if (compare > 0) {
+            System.out.println("Length of first line is greater than length of second line");
         } else {
-            System.out.println("lines are not equal");
+            System.out.println("Both the lines are equal");
         }
     }
     public static void main(String[] args) {
@@ -37,6 +40,6 @@ public class LineComparison {
         int x4 = sc.nextInt();
         System.out.print("y4 : ");
         int y4 = sc.nextInt();
-        checkEquality(x1, y1, x2, y2, x3, y3, x4, y4);
+        checkComparison(x1, y1, x2, y2, x3, y3, x4, y4);
     }
 }
